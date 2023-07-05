deff63e81997c65552c936565f9d4801123bd6e5
AllanSantos-DV <allannascimentodossantos@gmail.com>
2023-07-05 16:15:33 -0300

    Adcionado bloco Try catch.

---
+++ src/ManipulationStrings.java
@@ -0,0 +1,126 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class ManipulationStrings {
+
+    private static final String EMPTY_STRING_ERROR = "Entrada Invalida.";
+
+    public static void main(String[] args) {
+        try {
+            System.out.println("1. Reverta a ordem das palavras nas frases, mantendo a ordem das palavras:");
+            String input1 = entradaUsuario("Informe a frase:");
+            String output1 = inverterPalavras(input1);
+            System.out.println(output1);
+
+            System.out.println("2. Remova todos os caracteres duplicados da string abaixo:");
+            String input2 = entradaUsuario("Informe a frase:");
+            String output2 = removerDuplicados(input2);
+            System.out.println(output2);
+
+            System.out.println("3. Encontre a substring palindroma mais longa na string abaixo:");
+            String input3 = entradaUsuario("Informe a string:");
+            String output3 = maiorPalindrome(input3);
+            System.out.println(output3);
+
+            System.out.println("4. Coloque em maiúscula a primeira letra de cada frase na string:");
+            String input4 = entradaUsuario("Informe a frase:");
+            String output4 = capitalizeFrase(input4);
+            System.out.println(output4);
+
+            System.out.println("5. Verifique se a string é um anagrama de um palindromo:");
+            String input5 = entradaUsuario("Informe a string:");
+            String output5 = anagramaPalindrome(input5);
+            System.out.println(output5);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static String entradaUsuario(String message) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(message);
+        String input = scanner.nextLine();
+        while (input == null || input.isEmpty()) {
+            System.out.println("Entrada inválida. Por favor, informe novamente:");
+            input = scanner.nextLine();
+        }
+        return input;
+    }
+
+    private static void validarEntrada(String valor) {
+        if (valor == null || valor.isEmpty()) {
+            throw new IllegalArgumentException(EMPTY_STRING_ERROR);
+        }
+    }
+
+    public static String inverterPalavras(String sentence) {
+        validarEntrada(sentence);
+
+        return Arrays.stream(sentence.split(" "))
+                .reduce((word1, word2) -> word2 + " " + word1)
+                .orElse("");
+    }
+
+    public static String removerDuplicados(String string) {
+        validarEntrada(string);
+
+        return string.chars()
+                .distinct()
+                .mapToObj(c -> String.valueOf((char) c))
+                .collect(Collectors.joining());
+    }
+
+    public static String maiorPalindrome(String string) {
+        validarEntrada(string);
+
+        Optional<String> longestPalindrome = IntStream.range(0, string.length())
+                .mapToObj(i -> IntStream.rangeClosed(i, string.length())
+                        .mapToObj(j -> string.substring(i, j)))
+                .flatMap(Function.identity())
+                .filter(ManipulationStrings::isPalindrome)
+                .max(Comparator.comparingInt(String::length));
+
+        return longestPalindrome.orElse("");
+    }
+
+    private static boolean isPalindrome(String string) {
+        int left = 0;
+        int right = string.length() - 1;
+
+        while (left < right) {
+            if (string.charAt(left) != string.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+
+        return true;
+    }
+
+    public static String capitalizeFrase(String string) {
+        validarEntrada(string);
+
+        return Arrays.stream(string.split("(?<=\\.\\s)|(?<=\\?\\s)"))
+                .map(sentence -> sentence.isEmpty() ? sentence : Character.toUpperCase(sentence.charAt(0)) + sentence.substring(1))
+                .collect(Collectors.joining());
+    }
+
+    public static String anagramaPalindrome(String string) {
+        validarEntrada(string);
+
+        Map<Character, Long> charFrequencyMap = string.chars()
+                .mapToObj(c -> (char) c)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        long oddCount = charFrequencyMap.values()
+                .stream()
+                .filter(count -> count % 2 != 0)
+                .count();
+
+        return oddCount <= 1 ? "true" : "false";
+    }
+
+}
